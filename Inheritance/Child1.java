1b1ac897034703a593065269cee2c6915ef7f31b
maxranjan <ranjandon912@gmail.com>
2024-06-12 15:01:17 +0530

    Modify Some Lines

--- Inheritance/Child1.java
+++ Inheritance/Child1.java
@@ -1,14 +1,21 @@
-class Base{
+class Base {
     private int x = 10;
 
-void show(){
-    System.out.println(x);
+    /*
+     * Private Data member cannot be access with child class.
+     * It can only access in Parent class and method is also define in Parent
+     * classes.
+     */
+    
+    void show() {
+        System.out.println(x);
+    }
 }
-}
+
 class Child1 extends Base {
     public static void main(String[] args) {
         Child1 c1 = new Child1();
         c1.show();
     }
-    
+
 }
