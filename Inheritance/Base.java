a5a388133c7659c16a058118b8a4ee57475577b9
maxranjan <ranjandon912@gmail.com>
2024-06-12 14:40:20 +0530

    Add Inheritance Code

---
+++ Inheritance/Base.java
@@ -0,0 +1,27 @@
+// Note : " Instance Data Member of parent class will be moved in child class"
+// Note : " Parent class Object never be created at Implicitly but it can be
+// created Explicetly"
+
+class Papa {
+    int x; // It is the Instance Data Member of Parents Clsss
+    int y; // It is the Instance Data Member of Parents Clsss
+
+    void show() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+
+class Base extends Papa {
+
+    void get(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+        Base c1 = new Base();
+        c1.get(10, 20);
+        c1.show();
+    }
+}
